7b2ce4629b1a32c651d03c245820f9aee4a580ae
Ed Salinas <edsalinas7b@yahoo.com>
2019-03-19 06:55:49 +0800

    Updates emoji and includes postman test script

--- spec/controllers/status_controller_spec.rb
+++ spec/controllers/status_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "API V1 Status Change", type: 'request' do
         :token => "xoxp-578938749364-580410928646-578939025172-a8478064ff3cb72d35146c953017a3c1", 
         :domain => "edoscorp",
         :status_text => "Busy in 8 mins",
-        :status_emoji => ":moon:"
+        :status_emoji => "😄"
       }
     end
 
